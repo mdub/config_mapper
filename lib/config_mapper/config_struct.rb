0532aad2f0d2adc3638f1a75483e853a23cd8e13
Mike Williams <mdub@dogbiscuit.org>
2015-09-25 16:30:47 +1000

    Enter the eigenclass!

--- lib/config_mapper/config_struct.rb
+++ lib/config_mapper/config_struct.rb
@@ -4,31 +4,35 @@ module ConfigMapper
   #
   class ConfigStruct
 
-    def self.property(name, type = nil, options = {}, &coerce_block)
+    class << self
 
-      # Handle optional "type" argument
-      if options.empty? && type.kind_of?(Hash)
-        options = type
-        type = nil
-      end
-      if type
-        coerce_block = method(type)
-      end
+      def property(name, type = nil, options = {}, &coerce_block)
 
-      defaults[name] = options[:default].freeze
-
-      attr_accessor(name)
-
-      if coerce_block
-        define_method("#{name}=") do |arg|
-          instance_variable_set("@#{name}", coerce_block.call(arg))
+        # Handle optional "type" argument
+        if options.empty? && type.kind_of?(Hash)
+          options = type
+          type = nil
         end
+        if type
+          coerce_block = method(type)
+        end
+
+        defaults[name] = options[:default].freeze
+
+        attr_accessor(name)
+
+        if coerce_block
+          define_method("#{name}=") do |arg|
+            instance_variable_set("@#{name}", coerce_block.call(arg))
+          end
+        end
+
       end
 
-    end
+      def defaults
+        @defaults ||= {}
+      end
 
-    def self.defaults
-      @defaults ||= {}
     end
 
     def initialize
