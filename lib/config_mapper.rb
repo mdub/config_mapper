8042bd9fcaeaefdf85bf48f24c1193103ea6a309
Mike Williams <mdub@dogbiscuit.org>
2015-12-03 22:39:29 +1100

    Inline and refactor AttributeMapping.

--- lib/config_mapper.rb
+++ lib/config_mapper.rb
@@ -1,4 +1,3 @@
-require "config_mapper/attribute_sink"
 require "config_mapper/object_as_hash"
 
 # Supports marshalling of plain-old data (e.g. loaded from
@@ -17,9 +16,49 @@ module ConfigMapper
   #
   def self.set(data, target)
     target = ObjectAsHash[target]
-    mapper = AttributeSink.new(target)
-    mapper.set_attributes(data)
-    mapper.errors
+    AttributeMapping.new(data, target).apply
+  end
+
+  # Sets attributes on an object, collecting errors
+  #
+  class AttributeMapping
+
+    def initialize(data, target)
+      @data = data
+      @target = target
+      @errors = {}
+    end
+
+    attr_reader :data
+    attr_reader :target
+    attr_reader :errors
+
+    # Set multiple attributes from a Hash.
+    #
+    def apply
+      data.each do |key, value|
+        set_attribute(key, value)
+      end
+      errors
+    end
+
+    private
+
+    # Set a single attribute.
+    #
+    def set_attribute(key, value)
+      if value.is_a?(Hash) && !target[key].nil?
+        nested_errors = ConfigMapper.set(value, target[key])
+        nested_errors.each do |nested_key, error|
+          errors[".#{key}#{nested_key}"] = error
+        end
+      else
+        target[key] = value
+      end
+    rescue NoMethodError, ArgumentError => e
+      errors[".#{key}"] = e
+    end
+
   end
 
 end
