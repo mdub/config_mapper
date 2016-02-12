f5068fe0afa06aaf53ec167090aa14d846cf5aaf
Mike Williams <mdub@dogbiscuit.org>
2016-02-12 21:55:25 +1100

    Move mapping to AbstractTarget.

--- lib/config_mapper.rb
+++ lib/config_mapper.rb
@@ -18,8 +18,7 @@ module ConfigMapper
     # @return [Hash] exceptions encountered
     #
     def set(data, target_object)
-      target = wrap_target(target_object)
-      AttributeMapping.new(data, target).apply
+      wrap_target(target_object).configure_with(data)
     end
 
     private
@@ -30,49 +29,6 @@ module ConfigMapper
       else
         ObjectTarget.new(target_object)
       end
-    end
-
-  end
-
-  # Sets attributes on an object, collecting errors
-  #
-  class AttributeMapping
-
-    def initialize(data, target)
-      @data = data
-      @target = target
-      @errors = {}
-    end
-
-    attr_reader :data
-    attr_reader :target
-    attr_reader :errors
-
-    # Set multiple attributes from a Hash.
-    #
-    def apply
-      data.each do |key, value|
-        set_attribute(key, value)
-      end
-      errors
-    end
-
-    private
-
-    # Set a single attribute.
-    #
-    def set_attribute(key, value)
-      attribute_path = target.path(key)
-      if value.is_a?(Hash) && !target.get(key).nil?
-        nested_errors = ConfigMapper.set(value, target.get(key))
-        nested_errors.each do |nested_path, error|
-          errors["#{attribute_path}#{nested_path}"] = error
-        end
-      else
-        target.set(key, value)
-      end
-    rescue NoMethodError, ArgumentError => e
-      errors[attribute_path] = e
     end
 
   end
--- lib/config_mapper/abstract_target.rb
+++ lib/config_mapper/abstract_target.rb
@@ -3,6 +3,54 @@ module ConfigMapper
   # Wrap an object to make it look more like a Hash.
   #
   class AbstractTarget
+
+    def configure_with(data)
+      AttributeMapping.new(data, self).apply
+    end
+
+  end
+
+  # Sets attributes on an object, collecting errors
+  #
+  class AttributeMapping
+
+    def initialize(data, target)
+      @data = data
+      @target = target
+      @errors = {}
+    end
+
+    attr_reader :data
+    attr_reader :target
+    attr_reader :errors
+
+    # Set multiple attributes from a Hash.
+    #
+    def apply
+      data.each do |key, value|
+        set_attribute(key, value)
+      end
+      errors
+    end
+
+    private
+
+    # Set a single attribute.
+    #
+    def set_attribute(key, value)
+      attribute_path = target.path(key)
+      if value.is_a?(Hash) && !target.get(key).nil?
+        nested_errors = ConfigMapper.set(value, target.get(key))
+        nested_errors.each do |nested_path, error|
+          errors["#{attribute_path}#{nested_path}"] = error
+        end
+      else
+        target.set(key, value)
+      end
+    rescue NoMethodError, ArgumentError => e
+      errors[attribute_path] = e
+    end
+
   end
 
 end
