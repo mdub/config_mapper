b46cdf322008729d03c3eb9f3312a463f879c4ab
Mike Williams <mdub@dogbiscuit.org>
2016-11-18 18:17:55 +1100

    Remove a layer of nesting.

--- spec/config_mapper_spec.rb
+++ spec/config_mapper_spec.rb
@@ -62,153 +62,149 @@ module Testy
 
 end
 
-describe ConfigMapper do
+describe ConfigMapper, ".configure_with" do
 
-  describe ".configure_with" do
+  let!(:errors) { described_class.configure_with(source_data, target) }
 
-    let!(:errors) { described_class.configure_with(source_data, target) }
+  context "with a simple Hash" do
 
-    context "with a simple Hash" do
+    let(:position) { Testy::Position.new }
+    let(:target) { position }
 
-      let(:position) { Testy::Position.new }
-      let(:target) { position }
-
-      let(:source_data) do
-        {
-          "x" => 1,
-          "y" => "juan",
-          "z" => 42
-        }
-      end
-
-      it "sets recognised attributes" do
-        expect(position.x).to eql(1)
-      end
-
-      it "records ArgumentErrors raised by setter-methods" do
-        expect(errors[".y"]).to be_a(ArgumentError)
-      end
-
-      it "records NoMethodErrors for unrecognised keys" do
-        expect(errors[".z"]).to be_a(NoMethodError)
-      end
-
+    let(:source_data) do
+      {
+        "x" => 1,
+        "y" => "juan",
+        "z" => 42
+      }
     end
 
-    context "with a nested Hash" do
-
-      context "if there is a writer method" do
-
-        let(:thing) { Testy::ThingWithAHash.new }
-        let(:target) { thing }
-
-        let(:source_data) do
-          {
-            "stuff" => {
-              "x" => 1,
-              "y" => 2
-            }
-          }
-        end
-
-        it "overwrites the Hash" do
-          expect(thing.stuff).to eql("x" => 1, "y" => 2)
-        end
-
-        it "records no errors" do
-          expect(errors).to be_empty
-        end
-
-      end
-
-      context "otherwise" do
-
-        let(:state) { Testy::State.new }
-        let(:target) { state }
-
-        let(:source_data) do
-          {
-            "position" => {
-              "x" => 1,
-              "y" => "juan"
-            }
-          }
-        end
-
-        it "sets recognised attributes" do
-          expect(state.position.x).to eql(1)
-        end
-
-        it "records errors raised by nested objects" do
-          expect(errors[".position.y"]).to be_a(ArgumentError)
-        end
-
-      end
-
+    it "sets recognised attributes" do
+      expect(position.x).to eql(1)
     end
 
-    context "with an Array of values" do
+    it "records ArgumentErrors raised by setter-methods" do
+      expect(errors[".y"]).to be_a(ArgumentError)
+    end
 
-      let(:thing) { Testy::ThingWithSubnets.new }
+    it "records NoMethodErrors for unrecognised keys" do
+      expect(errors[".z"]).to be_a(NoMethodError)
+    end
+
+  end
+
+  context "with a nested Hash" do
+
+    context "if there is a writer method" do
+
+      let(:thing) { Testy::ThingWithAHash.new }
       let(:target) { thing }
 
       let(:source_data) do
         {
-          :subnets => [
-            "subnet-1",
-            "subnet-2"
-          ]
+          "stuff" => {
+            "x" => 1,
+            "y" => 2
+          }
         }
       end
 
-      it "sets the Array value" do
-        expect(thing.subnets).to eql(%w(subnet-1 subnet-2))
+      it "overwrites the Hash" do
+        expect(thing.stuff).to eql("x" => 1, "y" => 2)
+      end
+
+      it "records no errors" do
+        expect(errors).to be_empty
       end
 
     end
 
-    context "when the target is a read-only collection" do
+    context "otherwise" do
 
-      let(:positions) { Testy::NamedPositions.new }
-      let(:target) { positions }
+      let(:state) { Testy::State.new }
+      let(:target) { state }
 
       let(:source_data) do
         {
-          "stan" => { "x" => 1, "y" => 2 },
-          "mary" => { "x" => 5, "y" => 6, "attitude" => "unknown" }
+          "position" => {
+            "x" => 1,
+            "y" => "juan"
+          }
         }
       end
 
-      it "maps onto the object found" do
-        expect(positions["stan"].x).to eq(1)
+      it "sets recognised attributes" do
+        expect(state.position.x).to eql(1)
       end
 
       it "records errors raised by nested objects" do
-        bad_attitude = '["mary"].attitude'
-        expect(errors.keys).to include(bad_attitude)
-        expect(errors[bad_attitude]).to be_a(NoMethodError)
+        expect(errors[".position.y"]).to be_a(ArgumentError)
       end
 
-      context "and the config data is an Array" do
+    end
 
-        let(:source_data) do
-          [
-            { "x" => 3, "y" => 4 },
-            { "x" => 5, "y" => 6, "attitude" => "unknown" }
-          ]
-        end
+  end
 
-        it "uses array index as a key" do
-          expect(positions[0].x).to eq(3)
-          expect(positions[0].y).to eq(4)
-        end
+  context "with an Array of values" do
 
-        it "records errors raised by nested objects" do
-          bad_attitude = '[1].attitude'
-          expect(errors.keys).to include(bad_attitude)
-          expect(errors[bad_attitude]).to be_a(NoMethodError)
-        end
+    let(:thing) { Testy::ThingWithSubnets.new }
+    let(:target) { thing }
 
+    let(:source_data) do
+      {
+        :subnets => [
+          "subnet-1",
+          "subnet-2"
+        ]
+      }
+    end
+
+    it "sets the Array value" do
+      expect(thing.subnets).to eql(%w(subnet-1 subnet-2))
+    end
+
+  end
+
+  context "when the target is a read-only collection" do
+
+    let(:positions) { Testy::NamedPositions.new }
+    let(:target) { positions }
+
+    let(:source_data) do
+      {
+        "stan" => { "x" => 1, "y" => 2 },
+        "mary" => { "x" => 5, "y" => 6, "attitude" => "unknown" }
+      }
+    end
+
+    it "maps onto the object found" do
+      expect(positions["stan"].x).to eq(1)
+    end
+
+    it "records errors raised by nested objects" do
+      bad_attitude = '["mary"].attitude'
+      expect(errors.keys).to include(bad_attitude)
+      expect(errors[bad_attitude]).to be_a(NoMethodError)
+    end
+
+    context "and the config data is an Array" do
+
+      let(:source_data) do
+        [
+          { "x" => 3, "y" => 4 },
+          { "x" => 5, "y" => 6, "attitude" => "unknown" }
+        ]
+      end
+
+      it "uses array index as a key" do
+        expect(positions[0].x).to eq(3)
+        expect(positions[0].y).to eq(4)
+      end
+
+      it "records errors raised by nested objects" do
+        bad_attitude = '[1].attitude'
+        expect(errors.keys).to include(bad_attitude)
+        expect(errors[bad_attitude]).to be_a(NoMethodError)
       end
 
     end
